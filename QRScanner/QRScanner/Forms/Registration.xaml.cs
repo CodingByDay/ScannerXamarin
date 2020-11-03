adb0039c4a0cd914b76fe3e1a64714abb3616242
krznv <krznv@unv.rs>
2020-11-03 21:16:57 +0100

    Adding project structure and WebApp class with failed references

---
+++ QRScanner/QRScanner/Forms/Registration.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QRScanner.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registration : ContentPage
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnRegistration(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new MainMenu());
+        }
+    }
+}
---
+++ QRScanner/QRScanner/Services/WebApp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRScanner.Services
+{
+    class WebApp
+    { // Class for defining the web app.
+        
+
+
+
+
+
+
+    }
+}
